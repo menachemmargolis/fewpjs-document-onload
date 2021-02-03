6e1b64db7954e98f08802b3b576709cbd1cfa768
menachem margolis <margolismendel5728@gmail.com>
2021-02-03 15:24:15 -0500

    commit tree b0c3377feb8cb7af5126a1580b76f4b11843adad

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// document.addEventListener('DOMcontentLoaded',messageForDom )
+
+// function messageForDom(event){
+//     console.log("the DOM has loaded")
+// }
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let text = document.getElementById("text")
+    text.textContent = "This is really cool!"
+    console.log(text)
+  }); 
